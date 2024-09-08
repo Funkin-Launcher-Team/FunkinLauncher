98bf86f65b7dc360d306ae5b454ba7089336cd21
Errico <165917617+toperri@users.noreply.github.com>
2024-09-08 21:57:17 +0200

    Update Bootstrap.js

--- src/Bootstrap.js
+++ src/Bootstrap.js
@@ -1,12 +1,1 @@
-var os = require('os');
-
-if (os.platform() === 'win32') {
-    var release = os.release();
-    var version = parseInt(release.split('.')[0]);
-    
-    if (version >= 10) {
-        var app = require('./Main');
-    } else {
-        process.exit(1203);
-    }
-}
+var app = require('./Main');
